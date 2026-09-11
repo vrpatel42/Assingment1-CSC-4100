9b83ae1564cd2298e152f9b0b2a6d71314dcd483
Vraj Patel <vrpatel42@tntech.edu>
2026-09-11 14:34:58 -0500

    changes to file kernel.c

--- kernel.c
+++ kernel.c
@@ -1,9 +1,40 @@
 #include "libos.h"
 
+int print_progress(unsigned int row,
+					unsigned int col,
+					const char *str,
+					int perc,
+					int max_perc);
+
+int strlen(const char *str)
+{
+	int length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return length;
+}
+
+void boot_win(void)
+{
+    print_to(11, 2, "+------------------------------------------------------+");
+    print_to(12, 2, "|                                                      |");
+    print_to(13, 2, "|  [OK] Console                                        |");
+    print_to(14, 2, "|  [-]  Memory                                         |");
+    print_to(15, 2, "|  [-]  Kernel                                         |");
+    print_to(16, 2, "|  [-]  Interrupts                                     |");
+    print_to(17, 2, "|  [-]  Processes                                      |");
+    print_to(18, 2, "|                                                      |");
+    print_to(19, 2, "|                                                      |");
+    print_to(20, 2, "|                                                      |");
+    print_to(21, 2, "+------------------------------------------------------+");
+}
+
 int main(void){
 	boot_win();
 	while(1)
-1	{
+1	;{
 		for (int perc =10; perc <=100; perc +=10)
 		{
 			print_progress(19,4, "Progress: ",perc, 10);
